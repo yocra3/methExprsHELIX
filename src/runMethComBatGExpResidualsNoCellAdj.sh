710c20b0c5171a690bd839885ea8f3e3f7bdb317
Carlos Ruiz <yocra3@gmail.com>
2018-12-14 16:51:18 +0100

    Add analyses without adjusting for cell counts

---
+++ src/runMethComBatGExpResidualsNoCellAdj.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+###############################################################################
+# Methylation vs Expression Analysis using:
+#   - Methylation: ComBat
+#   - Expression: SVA Residuals protecting age, sex, cohort and cell counts
+#   - Model: adjust for age, sex, cohort
+#   - All Samples
+#   - Autosome chromosomes
+###############################################################################
+
+## Define paths to omic files
+meth="results/preprocessFiles/Methylation_GRSet.RData"
+gexp="results/preprocessFiles/Expression_SE_residuals.RData"
+resFolder="results/MethComBatExpResidualsNoCellAdj"
+
+methInput="${resFolder}/methyInput.Rdata"
+gexpInput="${resFolder}/gexpInput.Rdata"
+
+## Create folder for result
+mkdir $resFolder
+
+## Generate Input data for analysis
+Rscript src/createInputFiles.R $meth $gexp $resFolder autosomes
+
+## Divide data for analysis
+Rscript src/divide_data.R $methInput $gexpInput $resFolder
+
+## Run linear models
+for i in {1..22}
+do
+  echo $i
+  Rscript src/runLinearModelSubset.R $resFolder chr$i nocell $resFolder
+done
