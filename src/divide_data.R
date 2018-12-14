fe641d4f66bc72a388a256b1931005ad94f7ec42
Carlos Ruiz <yocra3@gmail.com>
2018-12-14 11:44:21 +0100

    Solve bug

--- src/divide_data.R
+++ src/divide_data.R
@@ -22,8 +22,7 @@ exprs <- arg[2]
 load(exprs) # se
 
 # Check data consistency ####
-stopifnot(colnames(gset) == colnames(se), "Sample ids in methylation dataset must be equal to sample ids in expression dataset")
-
+stopifnot(colnames(gset) == colnames(se))
 out_fold <- arg[3]
 
 print("Data loaded")
