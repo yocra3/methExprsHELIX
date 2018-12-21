2c16aa790f6102f2f6c32b25d1c2a8fffde0bfa1
Carlos Ruiz <yocra3@hotmail.com>
2018-12-21 15:33:23 +0100

    update script

--- src/runNoCellAdjCluster.sh
+++ src/runNoCellAdjCluster.sh
@@ -51,7 +51,7 @@ mkdir $resFolder
   
 
 ## Generate Input data for analysis
-R CMD BATCH --vanilla src/createInputFiles.R '--args methy="$meth" exprs="$gexp" out_fold="$resFolder" autosomes'
+R CMD BATCH --vanilla src/createInputFiles.R '--args methy="$meth" exprs="$gexp" out_fold="$resFolder" autosomes' $resFolder/divide.out
 
 ## Divide data for analysis
 R CMD BATCH --vanilla src/divide_data.R $methInput $gexpInput $resFolder
