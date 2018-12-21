46c9c1b7765efc4f4464d3bb1a1497cca2efe2b7
Carlos Ruiz <yocra3@hotmail.com>
2018-12-21 18:56:17 +0100

    Upload simulation

---
+++ src/runNoCellFemaleSims.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+###############################################################################
+# Run linear models for all the different models
+###############################################################################
+
+#set the job name
+#SBATCH -J=nocellFemaleSims
+
+#set the number of CPUS per task
+#SBATCH --ntasks-per-node=16
+
+#set the memory
+##SBATCH --mem=3000
+
+# job output file information
+#SBATCH -o nocellFemale.out
+
+# job errors file
+#SBATCH -e nocellFemale.err
+
+# set the partition where the job will run
+#SBATCH --partition=normal
+ 
+# set the number of nodes
+#SBATCH --nodes=1
+ 
+# set max wallclock time
+#SBATCH --time=7:00:00
+ 
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=END
+ 
+# send mail to this address
+#SBATCH --mail-user= carlos.ruiz@isglobal.org
+
+module load R/3.5.1-foss-2018b
+
+
+## No cell adjusted Female
+echo No Cell Adjusted Female
+model="nocellStrat"
+resFolder="results/MethComBatExpResidualsNoCellAdjStrat/female/sim${SLURM_ARRAY_TASK_ID}"
+mkdir resFolder
+
+for i in {1..22}
+do
+  echo $i
+  R CMD BATCH '--args data_fold="'$resFolder'" chr="chr'$i'" model="'$model'" out_fold="'$resFolder'" ${SLURM_ARRAY_TASK_ID}' src/runLinearModelSubset.R $resFolder/modchr$i.out
+done
+R CMD BATCH '--args data_fold="'$resFolder'" chr="chrX" model="'$model'" out_fold="'$resFolder'" ${SLURM_ARRAY_TASK_ID}' src/runLinearModelSubset.R $resFolder/modchrX.out
